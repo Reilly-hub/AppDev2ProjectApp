aa62f625d5f9c6835de45d6c238759ad8a450073
Reilly-hub <79654020+Reilly-hub@users.noreply.github.com>
2022-08-29 16:52:43 -0400

    Initial commit

---
+++ AppDev2ProjectApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppDev2ProjectApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
